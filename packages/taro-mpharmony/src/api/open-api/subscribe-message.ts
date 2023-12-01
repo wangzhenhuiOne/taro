f852fbd42f9a013b5139fd9be6a1da786906b861
wangzhenhuiOne <1664002766@qq.com>
2023-12-01 17:30:57 +0800

    feat: 新增功能订阅消息

--- packages/taro-mpharmony/src/api/open-api/subscribe-message.ts
+++ packages/taro-mpharmony/src/api/open-api/subscribe-message.ts
@@ -2,12 +2,25 @@
  * 请求订阅消息
  * 
  * @canUse requestSubscribeMessage
- * @null_implementation
+ * @__object [tmplIds]
+ * @__success [errMsg]
  */
-export const requestSubscribeMessage = () =>
-  Promise.resolve({
-    errMsg: 'requestSubscribeMessage:ok',
-  })
+export const requestSubscribeMessage = (options) => {
+  try {
+    // @ts-ignore
+    native.requestSubscribeMessage(options).then((res) => {
+      if (res.errMsg === 'requestSubscribeMessage:ok') {
+        options.success?.(res)
+      } else {
+        options.fail?.(res)  
+      }
+      options.complete?.(res)
+    })
+  } catch (res) {
+    options.fail?.(res) 
+    options.complete?.(res)
+  }
+}
 
 /**
  * 订阅设备消息接口，调用后弹出授权框，用户同意后会允许开发者给用户发送订阅模版消息
